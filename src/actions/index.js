48c6f097f79266c54720229890ea4499892c9f73
Sohail <sohailmomin1@gmail.com>
2017-06-05 21:21:44 +0530

    adds axios for get request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = "188655186254dfd0f31dda411577072a";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export default function(city) {
+	const url = `${ROOT_URL}&q${city},us`; 
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}
