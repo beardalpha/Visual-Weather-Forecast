f0c01af2ccf6af1a31e1febca40e4b306b3ddaa3
Sohail <sohailmomin1@gmail.com>
2017-06-05 18:32:54 +0530

    creates the internal component state

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
 
 export default class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			term: ''
+		};
+
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+
+	onInputChange(e) {
+		var term = e.target.value;
+		this.setState({term})
+	}
 	render() {
 		return (
 			<form className="input-group">
-				<input />
+				<input
+					placeholder="get a 5 day forecast for your city"
+					className="form-control" 
+					value={this.state.term}
+					onChange={this.onInputChange} 
+				/>
 				<span className="input-group-btn">
 					<button type="submit" className="btn btn-secondary">Search</button>
 				</span>
